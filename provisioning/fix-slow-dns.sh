fb0c8695c7d4b2e57638047d49b394ec1513fd24
le-garff-yoann <pe.weeble@yahoo.fr>
2018-09-04 14:47:44 +0200

    fix(*.sh) scoping issues

--- provisioning/fix-slow-dns.sh
+++ provisioning/fix-slow-dns.sh
@@ -6,5 +6,6 @@ then
       # https://access.redhat.com/site/solutions/58625 (subscription required)
       # http://www.linuxquestions.org/questions/showthread.php?p=4399340#post4399340
   echo 'RES_OPTIONS="single-request-reopen"' >> /etc/sysconfig/network
+
   service network restart
 fi
--- provisioning/motd.sh
+++ provisioning/motd.sh
@@ -6,6 +6,7 @@ MOTD_FILE=/etc/motd
 PLATFORM_RELEASE=$(sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/redhat-release)
 PLATFORM_MSG=$(printf 'CentOS %s' "$PLATFORM_RELEASE")
 BUILT_MSG=$(printf 'built %s' $(date +%Y-%m-%d))
+
 printf '%0.1s' "-"{1..64} > $MOTD_FILE
 printf '\n' >> $MOTD_FILE
 printf '%2s%-30s%30s\n' " " "${PLATFORM_MSG}" "${BUILT_MSG}" >> ${MOTD_FILE}
