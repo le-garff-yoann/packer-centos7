73b1d25ec8659ad5c4d181b4048accbba7cc682c
le-garff-yoann <pe.weeble@yahoo.fr>
2019-01-31 11:03:51 +0100

    style(provisionning/motd.sh) display POSIX timestamp instead of Datetime in motd

--- provisioning/motd.sh
+++ provisioning/motd.sh
@@ -5,7 +5,7 @@ date > /etc/vagrant_box_build_date
 MOTD_FILE=/etc/motd
 PLATFORM_RELEASE=$(sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/redhat-release)
 PLATFORM_MSG=$(printf 'CentOS %s' "$PLATFORM_RELEASE")
-BUILT_MSG=$(printf 'built %s' $(date +%Y-%m-%d))
+BUILT_MSG=$(printf 'built %s' $(date +%s))
 
 printf '%0.1s' "-"{1..64} > $MOTD_FILE
 printf '\n' >> $MOTD_FILE
